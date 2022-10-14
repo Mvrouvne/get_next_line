96cd596ceea71ddb5b379abf152335ff155200b7
Marouane Chaibat <machaiba@e1r9p9.1337.ma>
2022-10-14 17:44:20 +0100

    yesssir

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: machaiba <machaiba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:29:47 by machaiba          #+#    #+#             */
-/*   Updated: 2022/10/13 16:01:10 by machaiba         ###   ########.fr       */
+/*   Updated: 2022/10/14 17:43:24 by machaiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,43 +15,37 @@
 void	*ft_memmove(void *dest, const void *src, size_t size)
 {
 	size_t	x;
-	size_t	y;
-	size_t	z;
 	char	*cdest;
 	char	*csrc;
-	char 	*temp;
 
-	y = ft_strlen(dest);
-	z = ft_strlen(src);
 	cdest = ((char *)dest);
 	csrc = ((char *)src);
 	x = 0;
-	if (dest < src)
+	if (dest > src)
 	{
-		while ( x < size)
+		while (len > 0)
+		{
+			cdest[size - 1] = csrc[size - 1];
+			x--;
+		}
+	}
+	else
+	{
+		while (x < size)
 		{
 			cdest[x] = csrc[x];
 			x++;
 		}
 	}
-	else
-	{
-		x = size - 1;
-		while (x >= 0)
-		{
-			cdest[x] =csrc[x];
-			x--;
-		}
-	}
-	return (y + z);
+	return (dest);
 }
 int main()
 {
 	char a[15] = "Hi ";
 	char b[15] = "Marouane";
 
-	//printf("%s\n", memmove(a, 0, 3));
-	//printf("%s\n", b);
-	printf("%s\n", ft_memmove(a, 0, 3));
+	printf("%s\n", memmove(a, b, sizeof(char) * 15));
 	printf("%s\n", b);
+	// printf("%s\n", ft_memmove(a, b, sizeof(char) * 15));
+	// printf("%s\n", b);
 }
